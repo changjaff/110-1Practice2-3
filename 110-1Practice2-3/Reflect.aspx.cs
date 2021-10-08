79f4173bb100d03e2758dea65d76529c93f42dd8
榆讓 張 <108111101@mail.aeust.edu.tw>
2021-10-08 17:46:16 +0800

    練習2-4

---
+++ 110-1Practice2-3/Reflect.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice2_3
+{
+    public partial class Reflect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            int M = 111;  //要判斷的數值
+            int M2 = 0; // 儲存原始數值
+            int rev_M = 0; //反轉後的數值
+            int sum = 0; //計算後的餘數
+
+            M2 = M;
+
+
+            while (M != 0)
+            {
+                sum = M % 10;
+                rev_M = rev_M * 10 + sum;
+                M = M / 10;
+            }
+
+            if (M2 == rev_M)
+                Response.Write("True");
+            else
+                Response.Write("Flase");
+
+
+        }
+    }
+}
